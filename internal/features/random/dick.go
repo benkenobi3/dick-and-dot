b46aa395171d69d12ec2b2f5ac65cf1245c277fa
Artem Mikhalev <ammikhalev@avito.ru>
2023-10-02 12:45:51 +0300

    made dick random range -5..15

--- internal/features/random/dick.go
+++ internal/features/random/dick.go
@@ -1,5 +1,7 @@
 package random
 
+import "math/rand"
+
 func GetNewLength(startLength int64) int64 {
-	return startLength + 1
+	return startLength + rand.Int63n(16) - 5 // todo: should we avoid 0 value?
 }
