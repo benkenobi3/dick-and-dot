ba87d56cdda01b954d7ed40c4fb476746ade35e4
Artem Mikhalev <ammikhalev@avito.ru>
2023-10-07 15:05:54 +0300

    give bigger cocks more often

--- internal/features/random/dick.go
+++ internal/features/random/dick.go
@@ -1,13 +1,21 @@
 package random
 
 import (
+	"math"
 	"math/rand"
 )
 
 func GetNewLength(startLength int64) int64 {
-	if startLength == 0 {
-		return rand.Int63n(11)
-	}
+	linearLengthInc := randFloat(-5, 5)
+	normalizedLength := sigmoidNormalization(linearLengthInc)
+	return startLength + int64(math.Round(normalizedLength)) // todo: should we avoid 0 value?
+}
 
-	return startLength + rand.Int63n(16) - 5 // todo: should we avoid 0 value?
+// randFloat in range [min..max)
+func randFloat(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func sigmoidNormalization(input float64) float64 {
+	return (3.5 / (0.225 + math.Pow(math.E-0.4, -0.9-input))) - 5
 }
